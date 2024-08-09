e7ac5c7651e0ea0dac748a3547da6f304ddb249a
Atick Faisal <atickfaisal@gmail.com>
2024-08-09 23:51:18 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import logo from "/logo.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div>
+                <a href="#" target="_blank">
+                    <img src={logo} className="logo" alt="App logo" />
+                </a>
+            </div>
+            <h1>React.js PWA</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
+}
+
+export default App;
